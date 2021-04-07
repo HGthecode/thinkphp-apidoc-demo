6907bdef2cb9f05c436c54d2a0d5e29370e6da42
hg-code <376401263@qq.com>
2021-04-07 15:36:00 +0800

    更新2.4.4演示

--- app/model/User.php
+++ app/model/User.php
@@ -7,15 +7,20 @@ use think\Model;
 use hg\apidoc\annotation\Field;
 use hg\apidoc\annotation\WithoutField;
 use hg\apidoc\annotation\AddField;
+use hg\apidoc\annotation\Param;
 
 class User extends BaseModel
 {
 
 
     /**
-     * @field("id,username,nickname")
-     * @addField("openid",type="string",default="abc",desc="微信openid")
-     * @addField("senkey",type="string",default="key",desc="微信key")
+     * @Field("id,name,nickname,sex,username")
+     * @AddField("openid",type="string",default="abc",desc="微信openid")
+     * @AddField("senkey",type="string",default="key",desc="微信key")
+     * @AddField("role",type="array",desc="重写role",
+     *     @Param ("name",type="string",desc="名称"),
+     *     @Param ("id",type="string",desc="id"),
+     * )
      */
     public function getInfo($id){
         $res = $this->get($id);
