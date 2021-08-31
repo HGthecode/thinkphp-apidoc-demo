e86575bb89a41e3a367b0091e0d579c277b1cbad
hg-code <376401263@qq.com>
2021-08-31 14:24:03 +0800

    更新前端

--- public/apidoc/service-worker.js
+++ public/apidoc/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5271146ee47b3689014b30c1cf69e925.js"
+  "precache-manifest.50857f671f06f60a02572cf4b18a0d1b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "apidoc"});
